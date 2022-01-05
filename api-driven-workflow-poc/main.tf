04f624d1cbed9356ff1c39dc9d67b04b679c9483
Manal Maroua Boucenna <manal_maroua.boucenna@g.enp.edu.dz>
2022-01-05 17:20:06 -0500

    Update main.tf

--- api-driven-workflow-poc/main.tf
+++ api-driven-workflow-poc/main.tf
@@ -8,12 +8,6 @@ terraform {
   }
 }
 
-variable "dummy-variable" {
-  type = any
-  default = ""
-  description = "This dummy variable is used to test the sentinel policy enforcing the presence of description on variables' block."
-}
-
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
   value = "Hello, World! We used the version api-driven workflow for this POC :) "
