1bc8e11be4431af9709b0f8e7dc6d955fc19fe1e
Manal Maroua Boucenna <manal_maroua.boucenna@g.enp.edu.dz>
2022-01-05 17:18:29 -0500

    Add files via upload

---
+++ version-control-workflow-poc/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "remote" {
+    organization = "YOUR-TFE-ORGANIZATION"
+
+    workspaces {
+      name = "version-control-workflow-poc"
+    }
+  }
+}
+
+variable "dummy-variable" {
+  type = any
+  default = ""
+  description = "This dummy variable is used to test the sentinel policy enforcing the presence of description on variables' block."
+}
+
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World! We used the version control workflow for this POC :) "
+}
