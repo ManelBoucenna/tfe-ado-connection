d2ddcd919d57932c9327e379c28791bc4b3eb617
Manal Maroua Boucenna <manal.boucenna@slalom.com>
2022-02-05 12:52:46 -0500

    Update main.tf

--- cli-workflow/main.tf
+++ cli-workflow/main.tf
@@ -1,11 +1,10 @@
-terraform {
-  backend "remote" {
-    organization = "YOUR-TFE-ORGANIZATION"
-
-    workspaces {
-      name = "cli-workflow"
-    }
-  }
+terraform { 
+    backend "remote"{ 
+       organization = "YOUR-TFE-ORGANIZATION" #REPLACE_ME
+       workspaces { 
+          name = "YOUR-TFE-WORKSPACE" #REPLACE_ME
+          } 
+       }
 }
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
